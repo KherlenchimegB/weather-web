9f209dcfd32df9447e75258ecc79611d0dc8e915
Kherlenchimeg <b.kherlenchimeg@gmail.com>
2025-05-21 03:28:30 -0700

    Update index.jsx

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,91 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import Daily from "@/components/Daily";
+import Night from "./night";
+import Day from "./day";
+import { useEffect, useState } from "react";
+// import { Search } from "lucide-react";
+import SearchBox from "@/components/Search";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const weatherApiKey = "899d9c2c0f5845838dc70138240912";
 
 export default function Home() {
+  const [weatherData, setWeatherData] = useState();
+  const [cityName, setCityName] = useState("Tokyo");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${cityName}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setWeatherData(data);
+        console.log("weather data", data);
+      });
+  }, []);
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="relative flex items-center justify-center h-screen w-screen border-none rounded-md">
+      <SearchBox />
+      {/* <Day /> */}
+      <div className="relative flex flex-col justify-center items-center w-1/2  h-full border-none rounded-xl bg-[#F3F4F6]">
+        <img
+          src="./orange.png"
+          alt="orange"
+          className="absolute left-[280px] top-[90px] z-0 "
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <Daily
+          date={weatherData?.forecast.forecastday[0].date}
+          cityName={weatherData?.location.name}
+          dailyNews={
+            weatherData?.forecast.forecastday[0].hour[14].condition.text
+          }
+          dailyCelsium={weatherData?.forecast.forecastday[0].hour[14].temp_c}
+          dayIcon={weatherData?.forecast.forecastday[0].hour[14].condition.icon}
+          isLight={true}
+        />
+      </div>
+      {/* <Night /> */}
+      <div
+        className="relative flex flex-col justify-center items-center w-1/2  h-full border-none rounded-xl bg-black-400
+         bg-no-repeat bg-cover bg-center "
+        style={{ backgroundImage: `url("./bg-black.png")` }}
+      >
+        <img
+          src="./purple.svg"
+          alt="purple"
+          className="absolute right-[280px] bottom-[50px] z-0 w-[180px]"
+        />
+        <Daily
+          date={weatherData?.forecast.forecastday[0].date}
+          cityName={weatherData?.location.name}
+          nightNews={
+            weatherData?.forecast.forecastday[0].hour[21].condition.text
+          }
+          nightCelsium={weatherData?.forecast.forecastday[0].hour[21].temp_c}
+          nightIcon={
+            weatherData?.forecast.forecastday[0].hour[21].condition.icon
+          }
+          isLight={false}
+        />
+      </div>
+      {/* circles, vector */}
+      <div className="flex relative justify-center items-center bg-white rounded-full z-0">
+        <img
+          src="./vector-left.png"
+          alt="vector"
+          className="absolute h-[100px]"
+        />
+        <img
+          src="./vector-right.png"
+          alt="vector"
+          className="absolute h-[100px]"
+        />
+      </div>
+      <div className="border border-gray-300 rounded-full w-[540px] -translate-x-1/2 -translate-y-1/2 h-[540px] absolute top-1/2 left-1/2 "></div>
+      <div className="border border-gray-300 rounded-full w-[340px] -translate-x-1/2 -translate-y-1/2 h-[340px] absolute top-1/2 left-1/2 "></div>
+      <div className="border border-gray-300 rounded-full w-[140px] -translate-x-1/2 -translate-y-1/2 h-[140px] absolute top-1/2 left-1/2 "></div>
+      <div className="border border-gray-300 rounded-full w-[940px] -translate-x-1/2 -translate-y-1/2 h-[940px] absolute top-1/2 left-1/2 z-0"></div>
     </div>
   );
 }
